2a6dc1b89d867938250220f829cbdcc9349005dd
Jim Mello <jmello@g.harvard.edu>
2014-08-03 11:04:49 -0400

    initial checkin

---
+++ app/views/assign.blade.php
@@ -0,0 +1,39 @@
+@extends('main')
+
+@section('title')
+	Life Coach, Inc.
+@stop
+
+@section('jumbo')
+	<blockquote>Assign tasks to your clients.</blockquote>
+@stop
+
+@section('body')
+	<div class="container">
+		<div class="panel panel-default">
+		<div class="panel-body">
+
+		<h2 id="tables-example">Assignments</h2>
+		<table class="table table-condensed">
+			<tr>
+				<th>Completed</th>
+				<th>Due Date</th>
+				<th>Title</th>
+				<th>Description</th>
+				<th>Attachment</th>
+			</tr>
+			<?php $assignments = Assignment::where('user_id', '=', Auth::user()->id)->get(); ?>
+			@foreach ($assignments as $assignment)
+			<tr>
+				<td>{{$assignment->completed}}</td>
+				<td>{{$assignment->duedate}}</td>
+				<td>{{$assignment->title}}</td>
+				<td>{{$assignment->description}}</td>
+				<td>{{$assignment->filename}}</td>
+			</tr>
+			@endforeach
+		</table>
+		</div>
+		</div>
+	</div>
+@stop
